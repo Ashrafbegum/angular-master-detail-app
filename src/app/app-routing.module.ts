c99ddcb1e29ef86e61fe4eda0f93af84ea563e79
Ashraf <ashrafunnisa931@gmail.com>
2021-08-13 17:42:52 +1000

    First commit
    Installed angular cli
    
    Created project structure
    
    Added componets and configured routing for these components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MasterComponent } from './components/master/master.component';
+import { DetailComponent } from './components/detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'master',
+    component: MasterComponent
+  },
+  {
+    path: 'detail',
+    component: DetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [MasterComponent, DetailComponent]
