ff1556b19378a91d4800836769d5573d4ff0c9b7
Ashraf <ashrafunnisa931@gmail.com>
2021-08-15 20:39:11 +1000

    Added a service component namely Application
    Created a Model to save application form data

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApplicationService } from 'src/app/service/application.service';
+import { ApplicationModel } from './application.model';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  applicationForm !: FormGroup;
+  applicationObj: ApplicationModel = new ApplicationModel();
+  constructor(private formbuilder: FormBuilder, private application: ApplicationService ) { }
 
   ngOnInit(): void {
+    this.applicationForm = this.formbuilder.group ({
+      appNumber: [''],
+      appType: [''],
+      amount: [''],
+      status: [''],
+      name: [''],
+      gender: ['']
+    })
   }
 
+  handleSubmit(){
+    //console.log(this.applicationForm.value)
+    this.applicationObj = Object.assign(this.applicationObj, this.applicationForm.value)
+    this.application.addApplication(this.applicationObj)
+    this.applicationForm.reset();
+  }
+  retrieve(){
+    this.application.getApplication()
+   }
+   remove(){
+    this.application.removeApplication()
+   }
 }
---
+++ src/app/service/application.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ApplicationModel } from '../components/detail/application.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicationService {
+
+  constructor() {
+   }
+  addApplication(applicationObj: ApplicationModel){
+    let applications: ApplicationModel[];
+    if(sessionStorage.getItem('Applications')) {
+      applications = JSON.parse(sessionStorage.getItem('Applications') || '{}');
+      applications = [...applications, applicationObj]
+    } else {
+      applications = [applicationObj]
+    }
+    sessionStorage.setItem('Applications', JSON.stringify(applications))
+   }
+   getApplication(){
+     console.log(sessionStorage.getItem('Applications'))
+   }
+   removeApplication(){
+    sessionStorage.removeItem('Applications');
+   }
+}
